511a6d0b84b70a9c373595daa5bd08f1c5f7179d
Matthias Gutsch <matthiasgutsch@MacBook-Pro-di-Matthias.local>
2020-12-16 09:25:48 +0100

    dd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NODES } from './tree/mock-nodes';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
   title = 'tree-flower';
+
+  nodes = NODES;
 }
